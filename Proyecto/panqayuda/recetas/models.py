ce843b9b5bdcdd3a6db0fba47e8ad3a72c0c91e7
daniel amezcua <dany_amezcua@live.com.mx>
2018-02-22 18:39:10 -0600

    "Crear Aplicación Materiales"

--- Proyecto/panqayuda/recetas/models.py
+++ Proyecto/panqayuda/recetas/models.py
@@ -5,6 +5,9 @@ class Receta(models.Model):
     nombre = models.CharField(max_length=100, null=True, blank=False)
     cantidad = models.IntegerField()
     duration = models.DurationField()
+    material = models.ManyToManyField('materiales.Material')
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
     deleted_at = models.DateTimeField(blank = True, null = True)
+
+
