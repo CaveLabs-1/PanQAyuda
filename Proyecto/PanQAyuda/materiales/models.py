d65a8cb7ec80203836c1bbc505297f1a07b2cd91
daniel amezcua <dany_amezcua@live.com.mx>
2018-02-22 18:38:34 -0600

    "Crear Aplicación Materiales"

---
+++ Proyecto/PanQAyuda/materiales/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Material(models.Model):
+    nombre = models.CharField(max_length=100, null=True, blank=False)
+    unidad = models.CharField(max_length=10, null=True, blank=False)
+    codigo = models.CharField(max_length=10, null=True, blank=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
