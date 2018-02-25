f42a2fb550f396fe594b061a0413249e31cc9876
David Ramirez <davidsrnfire@gmail.com>
2018-02-25 16:29:26 -0600

    Agregando Funcionalidad a la Forma

--- Proyecto/panqayuda/recetas/views.py
+++ Proyecto/panqayuda/recetas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Receta
 from .forms import RecetaForm
+from django.shortcuts import redirect
 #
 # # Create your views here.
 # def agregar_receta(request):
@@ -14,5 +15,12 @@ def lista_recetas(request):
     recetas = list(Receta.objects.all())
     return render(request, 'recetas/lista_recetas.html', {'recetas': recetas})
 def agregar_receta(request):
-    form = RecetaForm()
+    if request.method == "POST":
+        form = RecetaForm(request.POST)
+        if form.is_valid():
+            receta = form.save(commit=False)
+            receta.save()
+            # return redirect('recetas', pk=receta.pk)
+    else:
+        form = RecetaForm()
     return render(request, 'recetas/agregar_receta.html', {'form': form})
