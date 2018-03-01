e211e530d3845944da1e9d50ea7c526e6fbb03fc
a01701017@itesm.mx <a01701017@itesm.mx>
2018-03-01 11:27:13 -0600

    Mejorar views paquetes

--- Proyecto/panqayuda/paquetes/views.py
+++ Proyecto/panqayuda/paquetes/views.py
@@ -3,8 +3,9 @@ from .models import Paquete
 from .models import Recetas_por_paquete
 from recetas.models import Receta
 from .forms import FormPaquete, FormRecetasPorPaquete
-
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib import messages
+from django.urls import reverse
 
 #indice
 def lista_paquetes(request):
@@ -15,22 +16,32 @@ def lista_paquetes(request):
 def agregar_paquete(request):
     if request.method == 'POST':
         forma_post=FormPaquete(request.POST)
-        if form.is_valid():
-            form.save()
+        if forma_post.is_valid():
+            forma_post.save()
             messages.success(request, 'Se ha agregado el paquete al catálogo!')
+            return HttpResponseRedirect(reverse('paquetes:agregar_recetas_a_paquete', kwargs={'id_paquete':id_paquete}))
         else:
-            message.error(request, 'Hubo un error, no se agregó el paquete.')
-        return render(request, 'paquetes/agregar_paquete.html', forma_post)
+            messages.error(request, 'Hubo un error y no se agregó el paquete. Intentalo de nuevo.')
+            return HttpResponseRedirect(reverse('paquetes:agregar_paquete'))
     else:
         forma=FormPaquete()
-        return render(request, 'paquetes/agregar_paquete.html', forma)
+        return render(request, 'paquetes/agregar_paquete.html', {'forma':forma})
 
 #agregar recetas a paquete
-def agregar_recetas_a_paquete():
-    return render(request, 'paquetes/agregar_recetas_a_paquete.html')
+def agregar_recetas_a_paquete(request, id_paquete):
+    paquete = get_object_or_404(Paquete, id=id_paquete)
+    forma = FormRecetasPorPaquete()
+    recetas = Receta.objects.filter(deleted_at = null, paquete = paquete)
+    return render(request, 'paquetes/agregar_recetas_a_paquete.html', {'recetas': recetas, 'forma':forma, 'recetas':recetas})
 
-#detalle de paquete
-def paquete():
-    return render(request, 'paquetes/paquete.html')
+def agregar_receta_a_paquete(request):
+    if request.method == 'POST':
+        html = 'hello'
+        return HttpResponse(html)
+
+def paquete(request, id_paquete):
+    paquete = get_object_or_404(Paquete, id=id_paquete)
+    recetas = recetas = Receta.objects.filter(deleted_at = null, paquete = paquete)
+    return render(request, 'paquetes/paquete.html', {'paquete': paquete, 'recetas': recetas})
 
 # Create your views here.
--- Proyecto/panqayuda/static/js/nav.js
+++ Proyecto/panqayuda/static/js/nav.js
@@ -1,8 +1,1 @@
 
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.tooltipped').tooltip();
-    $('.button-collapse').sidenav()({
-        draggable: true,
-    })
-});
