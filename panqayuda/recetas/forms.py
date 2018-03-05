c04c11a50807993244ae8d3431e0f672daf91cea
daniel amezcua <dany_amezcua@live.com.mx>
2018-03-05 12:42:19 -0600

    Testing y corrección de defectos US27

--- panqayuda/recetas/forms.py
+++ panqayuda/recetas/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Receta, RelacionRecetaMaterial
+from django.core.exceptions import ValidationError
 
 
 class RecetaForm(ModelForm):
@@ -8,8 +9,37 @@ class RecetaForm(ModelForm):
         model = Receta
         fields = ('nombre', 'cantidad', 'duration')
 
+    def clean_nombre(self):
+        nombre=self.cleaned_data['nombre']
+
+        #Buscar recetas que tengan el mismo nombre y que estén disponibles. La consulta no es case-sensitive.
+        receta_query = Receta.objects.filter(nombre__iexact=nombre).exclude(status=0)
+        if receta_query.count() == 0:
+            return nombre
+        else:
+            raise ValidationError("Esta receta ya existe")
+
 
 class MaterialRecetaForm(ModelForm):
     class Meta:
         model = RelacionRecetaMaterial
-        fields = ('material', 'cantidad')
+        fields = ('receta','material', 'cantidad')
+
+        error_messages = {
+            'cantidad': {
+                'required': "Debes seleccionar una cantidad.",
+            },
+            'material':{
+                'required': "Debes seleccionar un material."
+            }
+        }
+
+    def clean_material(self):
+        material = self.cleaned_data['material']
+        receta = self.cleaned_data['receta']
+        #Buscar si el material ya está en la receta
+        material_query = RelacionRecetaMaterial.objects.filter(receta=receta).filter(material=material).exclude(status=0)
+        if material_query.count() == 0:
+            return material
+        else:
+            raise ValidationError("El material seleccionado ya está en la receta.")
