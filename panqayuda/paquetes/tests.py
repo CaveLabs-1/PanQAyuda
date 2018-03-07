ad3c867d49bc3016d8be84391e1ad1eca5dbd0a3
David Ramirez <davidsrnfire@gmail.com>
2018-03-07 13:02:19 -0600

    Empezar Testing

--- panqayuda/paquetes/tests.py
+++ panqayuda/paquetes/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from paquetes.models import Paquete
 # Create your tests here.
+
+class TestBorrarPaqueteCatalogo(TestCase):
+
+    def crear_paquete(self):
+        return Paquete.objects.create(nombre="Paquete de Prueba", precio=12.0)
+
+    def test_borrado(self):
+        self.assertEqual(Paquete.objects.count(), 0)
+
+        data = {'nombre': "Paquete de Prueba", 'precio': 12}
+        self.client.post(reverse('paquetes:agregar_paquete'), data)
+
+        self.assertEqual(Paquete.objects.count(), 1)
+
+        self.client.post(reverse('paquetes:borrar_paquete'), data)
+        self.assertEqual(Paquete.objects.count(), 0)
