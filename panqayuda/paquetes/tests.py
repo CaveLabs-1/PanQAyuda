390ed66b8ead95b50f549b3f2d086cea9f3f34a5
David Ramirez <davidsrnfire@gmail.com>
2018-03-08 19:35:38 -0600

    Terminar tests

--- panqayuda/paquetes/tests.py
+++ panqayuda/paquetes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from paquetes.models import Paquete
+from django.urls import reverse
 # Create your tests here.
 
 class TestBorrarPaqueteCatalogo(TestCase):
@@ -7,13 +8,29 @@ class TestBorrarPaqueteCatalogo(TestCase):
     def crear_paquete(self):
         return Paquete.objects.create(nombre="Paquete de Prueba", precio=12.0)
 
-    def test_borrado(self):
+    def test_ac_26_1(self):
         self.assertEqual(Paquete.objects.count(), 0)
-
-        data = {'nombre': "Paquete de Prueba", 'precio': 12}
-        self.client.post(reverse('paquetes:agregar_paquete'), data)
-
+        p = self.crear_paquete()
+        # data = {'nombre': "Paquete de Prueba", 'precio': 12}
+        # self.client.post(reverse('paquetes:agregar_paquete'), data)
         self.assertEqual(Paquete.objects.count(), 1)
 
-        self.client.post(reverse('paquetes:borrar_paquete'), data)
-        self.assertEqual(Paquete.objects.count(), 0)
+        # self.client.post(reverse('paquetes:borrar_paquete'), p.id)
+        self.client.get(reverse('paquetes:borrar_paquete', kwargs={'id_paquete':p.id}))
+        self.assertEqual(Paquete.objects.count(), 1)
+
+
+    def test_ac_26_2(self):
+        p = self.crear_paquete()
+        resp = self.client.get(reverse('paquetes:lista_paquetes'))
+        self.assertEqual(len(resp.context['paquetes']),1)
+        self.client.get(reverse('paquetes:borrar_paquete', kwargs={'id_paquete':p.id}))
+        resp = self.client.get(reverse('paquetes:lista_paquetes'))
+        self.assertEqual(len(resp.context['paquetes']),0)
+
+
+    def test_ac_26_3(self):
+        p = self.crear_paquete()
+        self.client.get(reverse('paquetes:borrar_paquete', kwargs={'id_paquete':p.id}))
+        resp = self.client.get(reverse('paquetes:lista_paquetes'))
+        self.assertEqual(Paquete.objects.filter(deleted_at__isnull=True).count(), 0)
