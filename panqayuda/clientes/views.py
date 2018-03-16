8c8e30c9438518586692f858536daa7839600740
David Ramirez <davidsrnfire@gmail.com>
2018-03-16 12:19:16 -0600

    Terminar Vista

--- panqayuda/clientes/views.py
+++ panqayuda/clientes/views.py
@@ -16,3 +16,17 @@ def clientes(request):
         forma = FormCliente()
         clientes =  Cliente.objects.filter(deleted_at__isnull=True)
         return render (request, 'clientes/clientes.html', {'forma': forma, 'clientes': clientes})
+
+
+def editar_cliente(request, id_cliente):
+    cliente = get_object_or_404(Cliente, pk=id_cliente)
+    if request.method == "POST":
+        form = FormCliente(request.POST or None, instance=cliente)
+        if form.is_valid():
+            cliente = form.save()
+            cliente.save
+            messages.success(request, 'Se ha editado el cliente exitosamente!')
+            return render(request, 'clientes/cliente.html', {'cliente': cliente})
+    else:
+        form = FormCliente()
+    return render(request, 'clientes/editar_cliente.html', {'form': form, 'cliente': cliente})
