a2e68d3f0ff125dfb054c82a43b7cf4be8be8f76
Manuel Flores <a01206898@itesm.mx>
2018-03-21 13:41:49 -0600

    Agregar test de vista

--- panqayuda/clientes/tests.py
+++ panqayuda/clientes/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+import datetime
+from django.shortcuts import render
+from clientes.models import Cliente
 
+class TestEditarCliente(TestCase):
+
+    #Existe la vista
+    def test_existe_vista(self):
+        Cliente.objects.create(id=1, nombre="Bancomer", telefono=4151043944, direccion="calle 100 corazones", rfc=45321343, razon_social="Somos unos cracks", email="ejemplo@hotmail.com")
+        resp = self.client.get(reverse('clientes:editar_cliente', kwargs={'id_cliente':1}))
+        self.assertEqual(resp.status_code, 200)
+
+    
 # Create your tests here.
