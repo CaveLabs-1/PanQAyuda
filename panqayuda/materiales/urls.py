a70d7214010c5e99e93f51f1a5aee2fffb117e3b
David Ramirez <davidsrnfire@gmail.com>
2018-04-02 19:03:59 -0500

    /US12_ViewMateriaPrima: Auto stash before merge of "david/US12_ViewMateriaPrima" and "origin/develop"

--- panqayuda/materiales/urls.py
+++ panqayuda/materiales/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 app_name = 'clientes'
 
 urlpatterns = [
-    path('', views.materiales, name='materiales'),
+    path('', views.lista_materiales_inventario, name='lista_materiales_inventario'),
+    path('lista_materiales', views.materiales, name='materiales'),
+    path('materiales_por_catalogo/', views.materiales_por_catalogo, name='materiales_por_catalogo'),
     # path('editar_material/<int:id_cliente>', views.editar_cliente, name='editar_cliente'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
