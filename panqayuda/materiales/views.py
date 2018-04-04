824281530365927fda0506198ac7b53c297f80c0
Rodolfo <rudy.7.mtz@gmail.com>
2018-04-04 09:51:56 -0500

    Añadir view unidades comentada

--- panqayuda/materiales/views.py
+++ panqayuda/materiales/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render,reverse
+from django.shortcuts import render
 from .forms import MaterialForm
 from .models import Material
-from django.contrib import messages
-from django.http import HttpResponseRedirect
+from panqayuda.decorators import group_required
 
 
 # Create your views here.
 
+@group_required('admin')
 def materiales(request):
     if request.method == 'POST':
         forma_post = MaterialForm(request.POST)
@@ -21,3 +21,19 @@ def materiales(request):
         forma = MaterialForm()
         materiales =  Material.objects.filter(deleted_at__isnull=True)
         return render (request, 'materiales/materiales.html', {'forma': forma, 'materiales': materiales})
+
+# @group_required('admin')
+# def unidades(request):
+#     if request.method == 'POST':
+#         forma_post = FormUnidad(request.POST)
+#         if forma_post.is_valid():
+#             forma_post.save()
+#             messages.success(request, 'Se ha agregado una nueva unidad.')
+#         else:
+#             messages.error(request, 'Hubo un error, inténtalo de nuevo.')
+#
+#         return HttpResponseRedirect(reverse('materiales:unidades'))
+#     else:
+#         forma = FormUnidad()
+#         unidades =  Unidad.objects.filter(deleted_at__isnull=True)
+#         return render (request, 'materiales/unidades.html', {'forma': forma, 'unidades': unidades})
