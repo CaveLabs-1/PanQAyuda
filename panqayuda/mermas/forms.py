298f5fd716f64dc524327122a151a630df1595cf
Rodolfo <rudy.7.mtz@gmail.com>
2018-04-11 12:57:03 -0500

    Añadir view agregar_merma_material

--- panqayuda/mermas/forms.py
+++ panqayuda/mermas/forms.py
@@ -1,10 +1,24 @@
 from django.forms import ModelForm
 from django import forms
 from .models import MermaPaquete
+from .models import MermaMaterial
 
 class MermaPaqueteForm(ModelForm):
     class Meta:
         model = MermaPaquete
+        fields = (
+            'nombre',
+            'cantidad',
+            'fecha',
+            'descripcion'
+        )
+        widget = {
+            'fecha': forms.DateTimeField(),
+        }
+
+class MermaMaterialForm(ModelForm):
+    class Meta:
+        model = MermaMaterial
         fields = (
             'nombre',
             'cantidad',
--- panqayuda/mermas/urls.py
+++ panqayuda/mermas/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('lista_mermas/agregar_merma_paquetes', views.agregar_merma_paquetes, name='agregar_merma_paquetes'),
     path('lista_mermas_receta/', views.lista_mermas_receta, name='lista_mermas_receta'),
     path('lista_mermas_material/', views.lista_mermas_material, name='lista_mermas_material'),
+    path('lista_mermas_material/agregar_merma_materiales', views.agregar_merma_materiales, name='agregar_merma_materiales'),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
