f69ea85d48d1bebba1ddf3d26ec0580be984d136
Manuel Flores <a01206898@itesm.mx>
2018-04-10 19:47:01 -0500

    Agregar Tests

--- panqayuda/compras/admin.py
+++ panqayuda/compras/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Compra
+from .models import Compra, RelacionCompraMaterial
 
 admin.site.register(Compra)
+admin.site.register(RelacionCompraMaterial)
 # Register your models here.
