9ca8f6db265f7e25ede5b7da24cef4e8e2dcb122
daniel amezcua <dany_amezcua@live.com.mx>
2018-04-26 21:35:40 -0500

    Cambiar lenguaje a español POR DIOS

--- panqayuda/panqayuda/settings.py
+++ panqayuda/panqayuda/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-MX'
 
 TIME_ZONE = 'America/Mexico_City'
 
@@ -149,6 +149,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/ordenes'
 LOGIN_URL = '/ordenes'
 LOGOUT_REDIRECT_URL = 'login'
+
+
+
 
 # class DisableMigrations(object):
 #
