8d0516d4e62c84d93ec14f0abd022e5f1338e111
Rodolfo <rudy.7.mtz@gmail.com>
2018-05-02 10:55:48 -0500

    Corregir lenguaje DataTables

--- panqayuda/static/js/init.js
+++ panqayuda/static/js/init.js
@@ -25,15 +25,35 @@ function initDataTable(){
   data_table = $('#table_id').DataTable({
     dom: 'Bfrtip',
     buttons: ['excel'],
-    language:{
-      url: "//cdn.datatables.net/plug-ins/1.10.13/i18n/Spanish.json" //Cambia idioma a español
-    },
+    // language:{
+    //   "search":         "Search:",
+    //   url: "//cdn.datatables.net/plug-ins/1.10.13/i18n/Spanish.json" //Cambia idioma a español
+    // },
     // "scrollY": (window.innerHeight - 500) + "px",
     paging: true,
       "oLanguage": {
-
-        "sSearch": "Buscar sobre todos los campos presentes:"
-
+        	"sProcessing":     "Procesando...",
+        	"sLengthMenu":     "Mostrar _MENU_ registros",
+        	"sZeroRecords":    "No se encontraron resultados",
+        	"sEmptyTable":     "Ningún dato disponible en esta tabla",
+        	"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        	"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+        	"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+        	"sInfoPostFix":    "",
+        	"sSearch":         "Buscar en todos los camos presentes:",
+        	"sUrl":            "",
+        	"sInfoThousands":  ",",
+        	"sLoadingRecords": "Cargando...",
+        	"oPaginate": {
+        		"sFirst":    "Primero",
+        		"sLast":     "Último",
+        		"sNext":     "Siguiente",
+        		"sPrevious": "Anterior"
+        	},
+        	"oAria": {
+        		"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        		"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+	         }
       },
     columnDefs: [
            {
